ec950523f3af5b01708a4b99d8a6b4598a748a86
Joachim Ruhs <postmaster@joachim-ruhs.de>
2020-12-15 17:03:52 +0100

    Update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -3,33 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$_EXTKEY = 'myflat';
-
-/*
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Availabilityform',
-	'MyFlat (availability form)'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Availabilitycheck',
-	'MyFlat (availability check)'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Multirowcalendar',
-	'MyFlat (multirow calendar)'
-);
-*/
-
-
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'MyFlat');
-
-
-
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_myflat_domain_model_flat', 'EXT:myflat/Resources/Private/Language/locallang_csh_tx_myflat_domain_model_flat.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_myflat_domain_model_flat');
