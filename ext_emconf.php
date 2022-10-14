2c92a22c0efee319f6cc6165708bb05a6c29bbd7
Joachim Ruhs <postmaster@joachim-ruhs.de>
2022-10-14 12:11:49 +0200

    Version 0.7.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
   'state' => 'beta',
   'uploadfolder' => true,
   'clearCacheOnLoad' => 0,
-  'version' => '0.7.4',
+  'version' => '0.7.5',
   'constraints' => 
   array (
     'depends' => 
